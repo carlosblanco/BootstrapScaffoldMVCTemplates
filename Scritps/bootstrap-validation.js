e12bae8a129c10386c5d54118cdd41d017783ed9
carlosblanco <carlosomar2@gmail.com>
2012-11-08 11:53:27 -0700

    Validation script.

---
+++ Scritps/bootstrap-validation.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    $('span.field-validation-valid, span.field-validation-error').each(function () {
+        $(this).addClass('help-inline');
+    });
+
+    $('form').submit(function () {
+        if ($(this).valid()) {
+            $(this).find('div.control-group').each(function () {
+                if ($(this).find('span.field-validation-error').length == 0) {
+                    $(this).removeClass('error');
+                }
+            });
+        }
+        else {
+            $(this).find('div.control-group').each(function () {
+                if ($(this).find('span.field-validation-error').length > 0) {
+                    $(this).addClass('error');
+                }
+            });
+        }
+    });
+
+
+    $('form').each(function () {
+        $(this).find('div.control-group').each(function () {
+            if ($(this).find('span.field-validation-error').length > 0) {
+                $(this).addClass('error');
+            }
+        });
+    });
+
+});
+
+
+var page = function () {
+    //Update that validator
+    $.validator.setDefaults({
+        highlight: function (element) {
+            $(element).closest(".control-group").addClass("error");
+        },
+        unhighlight: function (element) {
+            $(element).closest(".control-group").removeClass("error");
+        }
+    });
+}();
